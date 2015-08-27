54b2620717b6e592a724e986c178498ac2f421fa
Sam Galison <samface@gmail.com>
2015-08-27 13:06:03 -0400

    this is just the beginning

---
+++ init.el
@@ -0,0 +1,38 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+(require 'cask "/usr/local/Cellar/cask/0.7.2_1/cask.el")
+(cask-initialize)
+(require 'pallet)
+(pallet-mode t)
+
+(add-to-list 'load-path "~/.emacs.d/lib")
+(add-to-list 'load-path "~/.emacs.d/custom")
+(load "general.el")
+(load "clojure-repls.el")
+(load "clojure.el")
+(load "paredit-conf.el")
+;; (load "prodigy.el")
+(load "web.el")
+(load "bindings.el")
+(load "highlight.el")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(ns-antialias-text nil)
+ '(shift-select-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco")))))
+(put 'upcase-region 'disabled nil)
